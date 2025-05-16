dccc4b6736c9abd471656f9fb663fa789419755e
StillMix <stillmixplaygames@gmail.com>
2025-05-16 23:44:33 +0300

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '../views/MainPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'MainPage',
+      component: MainPage,
+    },
+  ],
+})
+
+export default router
